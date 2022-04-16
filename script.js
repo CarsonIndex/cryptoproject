946be4b8400c1a9d6ce4c1a1e7fa5a1f99ecba5e
Carson <84282420+CarsonIndex@users.noreply.github.com>
2022-04-16 15:56:48 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("innerbox1").addEventListener("click", openAddInvestor);
+
+function openAddInvestor() {
+    window.open("carsonindex.github.io/cryptoproject/addinvestor", "_self")
+}
+
+function openAddCrypto() {
+    window.open("carsonindex.github.io/cryptoproject/addcrypto", "_self")
+}
+
+function openBuyInvestment() {
+    window.open("carsonindex.github.io/cryptoproject/buyinvestment", "_self")
+}
+
+function openSellInvestment() {
+    window.open("carsonindex.github.io/cryptoproject/sellinvestment", "_self")
+}
+
+function openViewInvestments() {
+    window.open("carsonindex.github.io/cryptoproject/viewinvestments", "_self")
+}
+
+function openViewInvestors() {
+    window.open("carsonindex.github.io/cryptoproject/viewinvestors", "_self")
+}
