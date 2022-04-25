9a485260075a1d49a88b24ddf68a1ab608d4d5fa
Carson <84282420+CarsonIndex@users.noreply.github.com>
2022-04-25 18:54:05 -0500

    addcrypto + addinvestor

--- addinvestor/script.js
+++ addinvestor/script.js
@@ -1,5 +1,24 @@
 document.getElementById("homebox").addEventListener("click", openHomePage);
+document.getElementById("IDbox").addEventListener("click", getAPI);
 
 function openHomePage() {
     window.location.href = 'https://CarsonIndex.github.io/cryptoproject';
+}
+
+function getAPI() {
+    var txt1 = document.getElementById("investorID").value
+    var txt2 = document.getElementById("investorName").value
+    var txt3 = document.getElementById("investorEmail").value
+    document.getElementById("resulttext").innerHTML = ""
+    fetch('https://dbms-uark-project.herokuapp.com/AddInvestor/' + txt1 + "/" + txt2 + "/" + txt3)
+        .then(response => response.text())
+        .then(data => myFunc(data));
+}
+
+function myFunc(Result) {
+    Result = Result.replaceAll("[", "")
+    Result = Result.replaceAll("]", "")
+    Result = Result.replaceAll("\"","")
+
+    document.getElementById("resulttext").innerHTML += Result
 }
